028d8066d85a8a0c17bce83a3427e8cbec0d67c0
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 19:39:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,14 @@
 def reverse_each_word(string)
   new = string.split(" ")
+  new2 = []
+  new.collect do |word|
+    new2.push(word.reverse())
+  end
+  return new2.join(" ")
+end
+
+def reverse_each_word(string)
+  new = string.split(" ")
   new.collect do |word|
     word = word.reverse()
   end
